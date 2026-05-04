19ba83130f708aea3e9b046e03c649eb58244ad5
Thomas Kaltz <tomkaltz@gmail.com>
2026-05-03 20:55:55 -0400

    upgrade packages

--- packages/tvw-frontend/app/plugins/trpc.client.ts
+++ packages/tvw-frontend/app/plugins/trpc.client.ts
@@ -1,10 +1,12 @@
 import { defineTkDcbTrpcWsPlugin } from '@tk-dcb/nuxt/runtime/trpc-ws-plugin'
 import type { TvwDomainRouter } from 'tvw-domain'
 
-const plugin = defineTkDcbTrpcWsPlugin<TvwDomainRouter>({
-  authStateKey: 'tvw-auth-token',
-  authStorageKey: 'tvw_auth_token',
-  debugLogTag: 'tvw trpc ws',
+/** Nuxt plugin transform only sees `defineNuxtPlugin` in this file (not inside tk-dcb). */
+export default defineNuxtPlugin({
+  name: 'trpc-client',
+  setup: defineTkDcbTrpcWsPlugin<TvwDomainRouter>({
+    authStateKey: 'tvw-auth-token',
+    authStorageKey: 'tvw_auth_token',
+    debugLogTag: 'tvw trpc ws',
+  }),
 })
-
-export default Object.assign(plugin, { _name: 'trpc-client' })
